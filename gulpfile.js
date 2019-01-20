920da4a5a4af417143b0631992eb4a9fdf44f80c
ovidiu <ovidiuo@localhost.localdomain>
2019-01-20 12:48:58 +0200

    fix fix security vulnerabilities

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var once = require('async-once');
 
-gulp.task('connect-dev', function(){
-    connect.server({
-        port: '',
+gulp.task('connect-dev', once(function () {
+    return connect.server({
+        port: '/',
         port: 8080
     });
-});
-gulp.task('default', ['connect-dev']);
+}));
 
-
+gulp.task('default', gulp.series('connect-dev'));
